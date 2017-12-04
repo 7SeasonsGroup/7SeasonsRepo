92fac36ad3350d140e39d634cba8f46751a98f7a
Ellie <SevenSeasonsGroup@hotmail.com>
2017-12-04 23:46:24 +0000

    commit one - test

---
+++ HaftFasl/HaftFasl/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="HaftFasl.MvcApplication" Language="C#" %>
---
+++ HaftFasl/HaftFasl/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(HaftFasl.Startup))]
+namespace HaftFasl
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
